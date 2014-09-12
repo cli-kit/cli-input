0f8b54d34d0303febf8921266a26bbdd266b303d
cyberfunk <hello@cyberfunk.org>
2014-09-12 15:09:05 +0800

    Assert on unacceptable confirm prompt value.

--- test/spec/confirm.js
+++ test/spec/confirm.js
@@ -4,7 +4,8 @@ var fsutil = require('../util/fsutil');
 var prompt = require('../..')
   , sequencer = require('../../lib/sequencer')
   , fsutil = require('../util/fsutil')
-  , sets = prompt.sets;
+  , sets = prompt.sets
+  , definitions = sets.definitions;
 
 describe('cli-input:', function() {
   this.timeout(5000);
@@ -12,6 +13,7 @@ describe('cli-input:', function() {
 
   it('should show binary confirm prompt (reject)', function(done) {
     var output = fsutil.getWriteStream('confirm-reject.txt');
+    var def = definitions.confirm.clone();
     var sequence = [
       {
         msg: 'are you sure? (y/n) ',
@@ -27,11 +29,12 @@ describe('cli-input:', function() {
     ];
     ps = prompt({name: mock.name, output: output});
     seq = new sequencer({ps: ps, output: output});
-    seq.run(sequence, sets.confirm);
+    seq.run(sequence, [def]);
   });
 
   it('should show binary confirm prompt (accept)', function(done) {
     var output = fsutil.getWriteStream('confirm-accept.txt');
+    var def = definitions.confirm.clone();
     var sequence = [
       {
         msg: 'are you sure? (y/n) ',
@@ -47,28 +50,34 @@ describe('cli-input:', function() {
     ];
     ps = prompt({name: mock.name, output: output});
     seq = new sequencer({ps: ps, output: output});
-    seq.run(sequence, sets.confirm);
+    seq.run(sequence, [def]);
   });
 
   it('should show binary confirm prompt (unacceptable)', function(done) {
     var output = fsutil.getWriteStream('confirm-unacceptable.txt');
+    var def = definitions.confirm.clone();
+
+    // have to switch off repeat to prevent maximum call stack error
+    def.repeat = false;
+
     var sequence = [
       {
         msg: 'are you sure? (y/n) ',
         input: 'ye',
         evt: 'unacceptable',
         cb: function(res, evt, value, options, ps) {
-          //expect(res.isPromptEqual()).to.eql(true);
-          //expect(value.result).to.eql('y');
-          //expect(value.accept).to.eql(true);
-          //expect(evt).to.eql('binary');
-          //done();
+          expect(res.isPromptEqual()).to.eql(true);
+          expect(value.result).to.eql('ye');
+          expect(value.accept).to.eql(null);
+          expect(evt).to.eql('unacceptable');
+          done();
         }
       }
     ];
+
     ps = prompt({name: mock.name, output: output});
     seq = new sequencer({ps: ps, output: output});
-    seq.run(sequence, sets.confirm);
+    seq.run(sequence, [def]);
   });
 
 });
