8323b3939bd0dd838220097a2d683efaa488d77d
cyberfunk <hello@cyberfunk.org>
2014-09-12 14:40:33 +0800

    Add default example.

--- example/default.js
+++ example/default.js
@@ -1,18 +1,15 @@
 var prompt = require('..')
   , sets = prompt.sets
   , definitions = sets.definitions;
+
 /**
- *  A simple question/answer prompt that expects an answer
- *  by setting required and repeat options.
+ *  A simple question/answer prompt that configures
+ *  a default value to use when no input is received.
  */
 var def = definitions.question.clone();
 def.parameters = ['how would you like that'];
-def.repeat = true;
-def.required = true;
+def.default = 'spicy';
 var ps = prompt();
-ps.on('empty', function() {
-  console.error('error: not a rhetorical question (answer required)');
-})
 ps.run([def], function(err, res) {
   if(err) console.error(err);
   if(res && res.map) {
